50525b2a93144031eb368649758e216edbd4a815
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-07 19:39:02 +0600

    truthy false done

---
+++ true-false.js
@@ -0,0 +1,110 @@
+// Falsy;
+// 0
+// "";
+// undefined
+// null
+// NaN
+// Truthy
+// '0'
+// ' '
+// [] 
+// {}
+const age = 4;
+
+if (age > 4) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+if (age) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+const name = "Solaiman Khan";
+
+if (name.length > 0) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+if (name.length) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+if (name) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+let name2 = "";
+
+if (name2) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+let name3 = " ";
+
+if (name3) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+let name4 = "0";
+
+if (name4) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+let name5;
+console.log(name5);
+
+if (name5) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+let name6 = null;
+
+if (name6) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+let name7 = NaN;
+
+if (name7) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+let name8 = {};
+
+if (name8) {
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
+
+
+

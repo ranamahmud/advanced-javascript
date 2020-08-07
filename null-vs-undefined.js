9657a4f297d9247b7ab81be13e0537ebdf59352a
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-07 20:41:15 +0600

    null vs undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,36 @@
+let pakhi;
+console.log(pakhi);
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+// const result = add(13, 82);
+// console.log(result);
+
+// function add(num1, num2) {
+//     console.log(num2);
+// }
+
+// const result = add(13);
+// console.log(result);
+
+
+function add(num1, num2=10) {
+    console.log(num2);
+}
+
+const result = add(13);
+console.log(result);
+
+const premik = {name: "smart dude",phone: 453827}
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+// null
+
+let ages = [2, 5, 9];
+console.log(ages[11]);

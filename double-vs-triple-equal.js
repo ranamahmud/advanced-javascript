cac1d1aa9f38dee95c38d532deaba2ea538a9182
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-07 20:50:51 +0600

    double vs triple equal done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,86 @@
+let first = 2;
+let second = 3;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+first = 2;
+second = 2;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+first = 2;
+second = "2";
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+first = 2;
+second = "2";
+
+if(first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+
+first = 2;
+second = true;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+first = 1;
+second = true;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+first = 1;
+second = false;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+first = 0;
+second = false;
+
+if(first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+
+first = 0;
+second = false;
+
+if(first === second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}

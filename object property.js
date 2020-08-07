96a85782159e2d4a6678d78f282c9642d49feaf9
Md Rana Mahmud <ranamd0@gmail.com>
2020-08-07 21:15:03 +0600

    object property map filter done

---
+++ object property.js
@@ -0,0 +1,20 @@
+const students = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 31, name: 'Mannaaaaaa'},
+    {id: 41, name: 'Moyouri'},
+    {id: 71, name: 'DeepJol'},
+]
+
+console.log(students);
+
+const names = students.map(s => s.name);
+console.log(names);
+
+const ids = students.map(s => s.id)
+console.log(ids);
+
+const bigger = students.filter(s=>s.id > 40);
+console.log(bigger);
+
+const biggerOne = students.find(s=>s.id > 40);
+console.log(biggerOne);
